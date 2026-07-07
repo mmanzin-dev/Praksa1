bda55c4d38590b082348bdcb017e8fa100226cff
Matko Manzin <matko@mononetz.com>
2026-07-07 11:44:37 +0200

    Use API to show data

--- angular-crash/src/app/services/task-service.ts
+++ angular-crash/src/app/services/task-service.ts
@@ -1,15 +1,15 @@
-import { Service } from '@angular/core';
+import { Service, inject, Injectable } from '@angular/core';
 import { Task } from '../Task';
-import { TASKS } from '../mock-tasks';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import {HttpClient, HttpHeaders} from '@angular/common/http'
 
 @Service()
 export class TaskService {
-    constructor() {}
+    private apiUrl = 'http://localhost:5000/tasks'
+
+    private http = inject(HttpClient);
 
     getTasks(): Observable<Task[]> {
-        const tasks = of(TASKS);
-        return tasks;
+        return this.http.get<Task[]>(this.apiUrl)
     }
 }
