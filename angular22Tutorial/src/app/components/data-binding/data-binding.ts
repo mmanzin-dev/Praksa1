955126580c0ecfdaf7c8aac75f700613eb62048c
Matko Manzin <matko@mononetz.com>
2026-07-08 10:09:55 +0200

    Data Binding i Event Binding

--- angular22Tutorial/src/app/components/data-binding/data-binding.ts
+++ angular22Tutorial/src/app/components/data-binding/data-binding.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-data-binding',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './data-binding.html',
   styleUrl: './data-binding.css',
 })
-export class DataBinding {}
+export class DataBinding {
+  productName: string = "Product1";
+  productPrice: number = 399;
+  maxAllowedChar: number = 5;
+  dynamicType: string = "checkbox";
+  isProductActive: boolean = true;
+
+  showAlert() {
+    alert("Angular 22 Alert");
+  }
+
+  onDropdownChange() {
+    alert("Dropdown has changed");
+  }
+
+  onMouseEnter() {
+    console.log("Mouse has entered the circle");
+  }
+
+  onMouseLeave() {
+    console.log("Mouse has left the circle");
+  }
+}
