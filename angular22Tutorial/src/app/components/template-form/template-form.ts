e82ec17770165485a593e1ceafc99c227ab09913
Matko Manzin <matko@mononetz.com>
2026-07-09 10:03:47 +0200

    Template form

--- angular22Tutorial/src/app/components/template-form/template-form.ts
+++ angular22Tutorial/src/app/components/template-form/template-form.ts
@@ -1,9 +1,10 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Form, FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-form',
-  imports: [FormsModule],
+  imports: [FormsModule, JsonPipe],
   templateUrl: './template-form.html',
   styleUrl: './template-form.css',
 })
@@ -15,4 +16,17 @@ export class TemplateForm {
     address: '',
     phone: ''
   };
+
+  onSaveCompany(formRef: NgForm) {
+    if (formRef.invalid) {
+      alert("Provide all the required details")
+    } else {
+      confirm("Do you want to save?")
+      const formValue = this.companyObj;
+    }
+  }
+
+  onPinChange() {
+    console.log("Pin Code changed")
+  }
 }
